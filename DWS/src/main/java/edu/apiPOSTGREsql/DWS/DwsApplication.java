c49d3565de1585714ba523adf898d940983a6c20
Ruben-DWS <csi24-rgarugo@colegioaltair.net>
2024-10-31 12:24:40 +0100

    Add files via upload
    Definitive Edition

---
+++ DWS/src/main/java/edu/apiPOSTGREsql/DWS/DwsApplication.java
@@ -0,0 +1,15 @@
+package edu.apiPOSTGREsql.DWS;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DwsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DwsApplication.class, args);
+		
+		
+		
+	}
+
+}
